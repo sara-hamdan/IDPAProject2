6ef5d166a1bd8bd1d1808d758a6854eaa2ef00dc
sara-hamdan <sarahhamdan53@gmail.com>
2022-04-25 14:49:44 +0300

    IDF scores

--- src/main/java/com/example/idpaproject2/other/MatrixRepresentation.java
+++ src/main/java/com/example/idpaproject2/other/MatrixRepresentation.java
@@ -17,6 +17,9 @@ import org.w3c.dom.traversal.TreeWalker;
 import org.xml.sax.SAXException;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class MatrixRepresentation {
 
@@ -49,39 +52,175 @@ public class MatrixRepresentation {
                     indexingNodes.add(tokenizedArr[j]);
 
                 }
-
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-
 
         return indexingNodes;
 
     }
 
-    public void getContext(Document document) {
+    public ArrayList<String> getMergedIndexingNodes(ArrayList<String> arr1, ArrayList<String> arr2) {
 
-        DocumentTraversal traversal = (DocumentTraversal) document;
+        ArrayList<String> mergedIndexingNodes = new ArrayList<>();
+        for(int i = 0; i < arr1.size(); i++) {
+            mergedIndexingNodes.add(arr1.get(i));
+        }
 
-        TreeWalker walker = traversal.createTreeWalker(document.getDocumentElement(),
-                NodeFilter.SHOW_ELEMENT, null, true);
+        for(int i = 0; i < arr2.size(); i++) {
+            mergedIndexingNodes.add(arr2.get(i));
+        }
 
-        ArrayList<String> structure = new ArrayList<>();
-        structure.add(document.getDocumentElement().getNodeName());
-        System.out.println(structure);
-        for (Node n = walker.firstChild(); n != null; n = walker.nextSibling()) {
+        return mergedIndexingNodes;
+    }
 
-            while(n.hasChildNodes()) {
+    public ArrayList<String> getMergedStructure(ArrayList<String> arr1, ArrayList<String> arr2) {
 
+        ArrayList<String> mergedStructure = new ArrayList<>();
+        for(int i = 0; i < arr1.size(); i++) {
+            mergedStructure.add(arr1.get(i));
+        }
+
+        for(int i = 0; i < arr2.size(); i++) {
+            mergedStructure.add(arr2.get(i));
+        }
+
+        return mergedStructure;
+    }
+
+
+
+    public ArrayList<String> getUniqueIndexingNodes(ArrayList<String> list){
+        Set<String> set = new LinkedHashSet<>(list);
+        return new ArrayList<String>(set);
+    }
+
+    public ArrayList<String> getContext(Element elem, StringBuilder path, ArrayList<String> structureList) {
+        final int pathLen = path.length();
+        if (pathLen != 0) // first iteration is pathLen = 0
+            path.append("/");
+        path.append(elem.getNodeName()); // we get the node name and append to the string builder
+        boolean hasChild = false;
+        for (Node child = elem.getFirstChild(); child != null; child = child.getNextSibling())
+            if (child.getNodeType() == Node.ELEMENT_NODE) {
+                hasChild = true;
+                getContext((Element)child, path, structureList);
 
             }
+        if (!hasChild) {
+            structureList.add(path.toString());
+        }
 
+        path.setLength(pathLen);
+        return structureList;
+    }
+
+    public ArrayList<String> getUniqueContext(ArrayList<String> list){
+        Set<String> set = new LinkedHashSet<>(list);
+        return new ArrayList<String>(set);
+    }
+
+
+    public HashMap<String, Integer> indexLabelling(ArrayList<String> indexingNodes) {
+
+        HashMap<String, Integer> mapper = new HashMap<>();
+
+        for(int i = 0; i < indexingNodes.size(); i++) {
+
+            mapper.put(indexingNodes.get(i), i);
 
         }
 
+        return mapper;
+
+    }
+
+    public HashMap<String, Integer> structureLabelling(ArrayList<String> structure) {
+
+        HashMap<String, Integer> mapper = new HashMap<>();
+
+        for(int i = 0; i < structure.size(); i++) {
+
+            mapper.put(structure.get(i), i);
+
+        }
+        return mapper;
+    }
+
+
+
+    public double getTFIDFScore(int TF, double IDF) {
+
+        double TFIDFScore = 1;
+
+        return TFIDFScore;
+
+    }
+
+    public void IDF_Matrix(Document document1, Document document2, ArrayList<String> mergedIndexingNodes, ArrayList<String> mergedStructure) {
+
+        Double[][] idf_matrix = new Double[getUniqueContext(mergedStructure).size()][getUniqueIndexingNodes(mergedIndexingNodes).size()];
+        ArrayList<String> indexingNodesOf1 = getUniqueIndexingNodes(getIndexingNodes(document1));
+        ArrayList<String> indexingNodesOf2 = getUniqueIndexingNodes(getIndexingNodes(document2));
+
+        ArrayList<String> structureOf1 = new ArrayList<>();
+        structureOf1 = getUniqueContext(getContext(document1.getDocumentElement(), new StringBuilder(), structureOf1));
+        ArrayList<String> structureOf2 = new ArrayList<>();
+        structureOf2 = getUniqueContext(getContext(document2.getDocumentElement(), new StringBuilder(), structureOf2));
+
+        for (int i = 0; i < getUniqueContext(mergedStructure).size(); i++) {
+            for (int j = 0; j < getUniqueIndexingNodes(mergedIndexingNodes).size(); j++) {
+                if (!findStructureOrIndexingNode(structureOf1, mergedStructure.get(i))) {
+                    idf_matrix[i][j] = 0.0;
+                } else if(!findStructureOrIndexingNode(indexingNodesOf1, mergedIndexingNodes.get(j))) {
+                        idf_matrix[i][j] = 0.0;
+                    }
+                else {
+                    NodeList nodeList1 = document1.getElementsByTagName(getUniqueContext(mergedStructure).get(i).split("/")[getUniqueContext(mergedStructure).get(i).split("/").length-1]);
+                    NodeList nodeList2 = document2.getElementsByTagName(getUniqueContext(mergedStructure).get(i).split("/")[getUniqueContext(mergedStructure).get(i).split("/").length-1]);
+                    for (int k = 0; k < nodeList1.getLength(); k++) {
+                        if (nodeList1.item(k).getTextContent().contains(getUniqueIndexingNodes(mergedIndexingNodes).get(j))) {
+                            idf_matrix[i][j] = Math.log(3/1);
+
+                        }
+
+                        else idf_matrix[i][j] = 0.0;
+
+                    }
+
+                    for (int l = 0; l < nodeList2.getLength(); l++) {
+                        if (nodeList2.item(l).getTextContent().contains(getUniqueIndexingNodes(mergedIndexingNodes).get(j))) {
+                            idf_matrix[i][j] = Math.log(3.0/2);
+                        }
+                    }
+
+                }
+            }
+
+        }
+        for (int i = 0; i < idf_matrix.length; i++) { //this equals to the row in our matrix.
+            for (int j = 0; j < idf_matrix[i].length; j++) { //this equals to the column in each row.
+                System.out.print(idf_matrix[i][j] + " ");
+            }
+            System.out.println(); //change line on console as row comes to end in the matrix.
+        }
+
+
+
+
+    }
+
+    public boolean findStructureOrIndexingNode(ArrayList<String> documentList, String indexingNode) {
+
+        for(int i = 0; i < documentList.size(); i++) {
+
+            if(indexingNode.equalsIgnoreCase(documentList.get(i))) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
-
--- src/main/java/com/example/idpaproject2/other/Testing.java
+++ src/main/java/com/example/idpaproject2/other/Testing.java
@@ -1,26 +1,82 @@
 package com.example.idpaproject2.other;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Testing {
 
     public static void main(String[] args) throws ParserConfigurationException {
-        //
+
         MatrixRepresentation mp = new MatrixRepresentation();
         Document document1 = mp.parseXML("src/doc1.xml");
-        ArrayList<String> indexingNodes = mp.getIndexingNodes(document1);
+        Document document2 = mp.parseXML("src/doc2.xml");
+        ArrayList<String> indexingNodes1 = mp.getIndexingNodes(document1);
+        ArrayList<String> indexingNodes2 = mp.getIndexingNodes(document2);
 
-        System.out.println(indexingNodes.size());
-        for(int i = 0; i < indexingNodes.size(); i++) {
-            System.out.print(indexingNodes.get(i) + " ");
+        for(int i = 0; i < indexingNodes1.size(); i++) {
+            System.out.println(indexingNodes1.get(i) + " ");
         }
 
-        mp.getContext(document1);
+        System.out.println();
+
+        for(int i = 0; i < indexingNodes2.size(); i++) {
+            System.out.print(indexingNodes1.get(i) + " ");
+        }
+
+        ArrayList<String> structureList1 = new ArrayList<>();
+        ArrayList<String> structureList2 = new ArrayList<>();
+        StringBuilder str1 = new StringBuilder();
+        StringBuilder str2 = new StringBuilder();
+        mp.getContext(document1.getDocumentElement(), str1, structureList1);
+        mp.getContext(document2.getDocumentElement(), str2, structureList2);
+
+        for(int i = 0; i < structureList1.size(); i++) {
+            System.out.println(structureList1.get(i) + " ");
+        }
+        System.out.println();
+
+        for(int i = 0; i < structureList2.size(); i++) {
+            System.out.println(structureList2.get(i) + " ");
+        }
+        System.out.println();
+
+
+
+
+        ArrayList<String> mergedIndexingNodes = mp.getMergedIndexingNodes(indexingNodes1, indexingNodes2);
+        HashMap<String, Integer> indexMapper = mp.indexLabelling(mp.getUniqueIndexingNodes(mergedIndexingNodes));
+
+        for (Map.Entry<String, Integer> e : indexMapper.entrySet())
+
+            // Printing key-value pairs
+            System.out.println(e.getKey() + " "
+                    + e.getValue());
+
+
+        ArrayList<String> mergedStructureList = mp.getMergedStructure(structureList1, structureList2);
+        HashMap<String, Integer> structureMapper = mp.structureLabelling(mp.getUniqueIndexingNodes(mergedStructureList));
+
+        for (Map.Entry<String, Integer> e : structureMapper.entrySet())
+
+            // Printing key-value pairs
+            System.out.println(e.getKey() + " "
+                    + e.getValue());
+
+
+        mp.IDF_Matrix(document2, document1, mergedIndexingNodes, mergedStructureList);
+
+
+    }
+
+
     }
 
 
 
-    }
+
+
